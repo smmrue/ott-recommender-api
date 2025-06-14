75cf86e47b1c9c84b3fd03be38e8e593ff9d3450
smmrue <psjung0826@naver.com>
2025-06-14 16:39:18 +0900

    로그 확인용

--- api/duksungAI.js
+++ api/duksungAI.js
@@ -12,13 +12,11 @@ export default async function handler(req, res) {
     if (req.method === "OPTIONS") {
         return res.status(200).end();
 }
-
+console.log("요청 body 확인:", req.body);
 const { ott, genre } = req.body;
 if (!ott || !genre) {
     return res.status(400).json({error:"ott와 장르 선택을 해야 합니다. "});
 } 
-
-
 try {
     const prompt = `
     OTT 플랫폼: ${ott}
